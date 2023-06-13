8c94aa1f38047e7634f8e03e889fa58ddd4a3a56
Leon-maker <ldefonrocque@gmail.com>
2023-06-13 12:37:06 +0200

    s

--- front-page.php
+++ front-page.php
@@ -34,7 +34,7 @@ $title_page = 'ACCUEIL';
         <div class="left-container">
       		<a href="thrive_wp/read-me" class="block text-center">README</a>
 		</div>
-		<h4>salut</h4>
+		<h4>salu</h4>
 
     </div>
 </section>
