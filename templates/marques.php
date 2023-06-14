bd8b01da2a1c51218d18e8aafefebf69df5f2216
mathias <mathias.duprat@live.com>
2023-06-14 11:50:01 +0200

    marque card

--- templates/marques.php
+++ templates/marques.php
@@ -17,12 +17,17 @@
     $query = new WP_Query($args);
     if ($query->have_posts()) :
         while ($query->have_posts()) : $query->the_post();
-            $img_marque = wp_get_attachment_image_src(get_field('image_marque'), 'full')[0]; 
+            $img_marque = wp_get_attachment_image_src(get_field('image_principale'), 'full')[0]; 
+
     ?>
     <div class="marque-card">
         <img class="marque-card-img" src="<?= $img_marque ?>" alt="Image d'illustration marque"/>
-        <h3 class="marque-card-title"><?php the_title(); ?></h3>
-
+        <div class="marque-card-content-container">
+            <h3 class="marque-card-title"><?php the_title(); ?></h3>
+            <div class="marque-card-container-paragraphe">
+                <p class="marque-card-paragraphe"><?php the_field('description'); ?></p>
+            </div>
+        </div>
     </div>
 
 
