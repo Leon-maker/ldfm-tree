2a852a48795d3eb86a8f4dd6561d8278b380f811
Leon-maker <ldefonrocque@gmail.com>
2023-06-15 11:33:41 +0200

    shortcode

--- front-page.php
+++ front-page.php
@@ -3,78 +3,12 @@
 
 // If the header needs to change a specific color with this template, then define it :
 //$header_color = '#000000';
-if ($header_color) {
-	get_header( '', array( 'header-color' => $header_color) ); 
-} else {
-	get_header();
-}
 
-$header_section_slug = 'home-page-header';
-$args = array(
-  'post_type'   => 'sections',
-  'name'        => $header_section_slug,
-  'post_status' => 'publish',
-  'numberposts' => 1
-);
 
-$query = new WP_Query($args);
+echo do_shortcode('[shortcode-home-section]'); 
 
-if ($query->have_posts()) {
-    $query->the_post();
-    // Variables
-    $title = get_the_title();
-    $section_id = get_the_ID();
-    if (has_post_thumbnail()) {
-        $thumbnail_data = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full');
-        $thumbnail_url = $thumbnail_data[0]; 
-    }
-    $slider = get_field('slider');
-    $slide_repeater = $slider['slide'];
-}
 
-?>
-
-<section class="home-section header-bkg header-slider no-margin-top no-margin-bottom">
-    <div class="header-wrapper">
-        <div class="swiper-container">
-            <div class="swiper-wrapper">
-                <?php
-                foreach ($slide_repeater as $slide) {
-                    ?> 
-                    <section class="padding-top-header swiper-slide" style="background-image: url('<?php echo $slide['image_de_la_slide']; ?>');" role="img" aria-label="Image d'illustration 1 de la première section de la page." class="illustration illu1" style="width: 100%;">
-                        <div class="text-center white">
-                            <h1>
-                                <?php echo $slide['titre_de_la_slide']; ?>
-                            </h1>
-                            <?php if (!empty($slide['sous-titre_de_la_slide'])) { ?>
-                                <h3>
-                                    <?php echo $slide['sous-titre_de_la_slide']; ?> 
-                                </h3>
-                            <?php } ?>
-                            <div class="header-button">
-                                <?php if (!empty($slide['bouton'])) { ?>
-                                    <a class="cta-secondary white" href="<?php echo $slide['bouton']['lien_bouton']; ?>"><?php echo $slide['bouton']['titre_bouton']; ?></a>
-                                <?php } ?>
-                            </div>
-                        </div>
-                    </section>
-                <?php } ?>
-            </div>
-            <div class="swiper-button-container left">
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
-            </div>
-            <div class="mouse-div">
-                <a class="cta-mouse white" href="">
-                    <span class="arrow">
-                    </span>
-                </a>
-            </div>   
-        </div>
-    </div>
-</section>
-
-<?php get_footer() ?>
+get_footer() ?>
 
 
 <!-- <?php
--- shortcodes/shortcode-home-section.php
+++ shortcodes/shortcode-home-section.php
@@ -1,57 +1,75 @@
 <?php
 
-if ($header_color) {
-	get_header( '', array( 'header-color' => $header_color) ); 
-} else {
-	get_header();
-}
-
-$header_section_slug = 'home-page-header';
-$args = array(
-  'post_type'   => 'sections',
-  'name'        => $header_section_slug,
-  'post_status' => 'publish',
-  'numberposts' => 1
-);
-
-$query = new WP_Query($args);
-
-if ($query->have_posts()) {
-    $query->the_post();
-    // Variables
-    $title = get_the_title();
-    $section_id = get_the_ID();
-    if (has_post_thumbnail()) {
-        $thumbnail_data = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full');
-        $thumbnail_url = $thumbnail_data[0]; 
-    }
-    $slider = get_field('slider');
-    $slide_repeater = $slider['slide'];
-}
-
-function SHORTCODE_HistoireSection()
+function SHORTCODE_HomeSection()
 {
     ob_start();
+
+    $header_section_slug = 'home-page-header';
+    $args = array(
+    'post_type'   => 'sections',
+    'name'        => $header_section_slug,
+    'post_status' => 'publish',
+    'numberposts' => 1
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) {
+        $query->the_post();
+        // Variables
+        $title = get_the_title();
+        $section_id = get_the_ID();
+        if (has_post_thumbnail()) {
+            $thumbnail_data = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full');
+            $thumbnail_url = $thumbnail_data[0]; 
+        }
+        $slider = get_field('slider');
+        $slide_repeater = $slider['slide'];
+    }
+
+    if ($header_color) {
+        get_header( '', array( 'header-color' => $header_color) ); 
+    } else {
+        get_header();
+    }
     ?>
 
-    <section class="padding-top-header" style="background-image: url('<?php echo $img_bkg; ?>');" role="img" aria-label="Image d'illustration 1 de la première section de la page." class="illustration illu1" style="width: 100%;">
-        <?php echo do_shortcode('[shortcode-fil_ariane type="white" title2="Boutique"]'); ?>
-        <div class="text-center white">
-            <h2>
-                Notre histoire
-            </h2>
-            <?php if (!empty($description)) { ?>
-                <h3>
-                    <?php echo $description; ?> 
-                </h3>
-            <?php } ?>
-            <div class="header-button">
-                <?php if (!empty($button1)) { ?>
-                    <a class="cta-third" href=""><?php echo $button1; ?></a>
-                <?php } ?>
-                <?php if (!empty($button2)) { ?>
-                    <a class="cta-third" href=""><?php echo $button2; ?></a>
-                <?php } ?>
+    <section class="home-section header-bkg header-slider no-margin-top no-margin-bottom">
+        <div class="header-wrapper">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <?php
+                    foreach ($slide_repeater as $slide) {
+                        ?> 
+                        <section class="padding-top-header swiper-slide" style="background-image: url('<?php echo $slide['image_de_la_slide']; ?>');" role="img" aria-label="Image d'illustration 1 de la première section de la page." class="illustration illu1" style="width: 100%;">
+                            <div class="text-center white">
+                                <h1>
+                                    <?php echo $slide['titre_de_la_slide']; ?>
+                                </h1>
+                                <?php if (!empty($slide['sous-titre_de_la_slide'])) { ?>
+                                    <h3>
+                                        <?php echo $slide['sous-titre_de_la_slide']; ?> 
+                                    </h3>
+                                <?php } ?>
+                                <div class="header-button">
+                                    <?php if (!empty($slide['bouton'])) { ?>
+                                        <a class="cta-secondary white" href="<?php echo $slide['bouton']['lien_bouton']; ?>"><?php echo $slide['bouton']['titre_bouton']; ?></a>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </section>
+                    <?php } ?>
+                </div>
+                <div class="swiper-button-container left">
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                </div>
+                <div class="mouse-div">
+                    <a class="cta-mouse white" href="">
+                        <span class="arrow">
+                        </span>
+                    </a>
+                </div>   
             </div>
         </div>
     </section>
@@ -60,4 +78,4 @@ function SHORTCODE_HistoireSection()
     return ob_get_clean();
 }
 
-add_shortcode('shortcode-histoire-section', 'SHORTCODE_HistoireSection');
+add_shortcode('shortcode-home-section', 'SHORTCODE_HomeSection');
