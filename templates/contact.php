152f5dd552a869cd4a18c4e2c99392bf51948dcd
Leon-maker <ldefonrocque@gmail.com>
2023-06-15 16:17:36 +0200

    contact

--- templates/contact.php
+++ templates/contact.php
@@ -7,8 +7,8 @@ $title_page = get_the_title(); ?>
 <?php 
 $img_bkg = wp_get_attachment_image_src(21512, 'full')[0];
 $title_page = 'Contactez-nous';
-$ArianeTitle = "Nos Marques";
-$ArianeLink1 = "https://influence-by-m.thrive-production.fr/nos-marques/";
+$ArianeTitle = "Contact";
+$ArianeLink1 = "https://influence-by-m.thrive-production.fr/contact/";
 
 echo do_shortcode('[shortcode-header-section img-bkg="' . $img_bkg . '" title="' . $title_page . '" link1="'. $ArianeLink1 .'" title1="'.$ArianeTitle.'"]'); 
 
@@ -27,8 +27,70 @@ echo do_shortcode('[shortcode-header-section img-bkg="' . $img_bkg . '" title="'
       Stet clita kasd gubergren, no sea takimata sanctus.
       </p>
     </div>
+    <div class="informations-pratiques full-width">
+      <h3>
+        Coordonnées
+      </h3>
+      <ul class="infos-container list-in-rows-border">
+        <li class="infos">
+          <p class="label">
+            Adresse
+          </p>
+          <p class="value boldy">
+            24 avenue Jean Perrin 33700 Mérignac
+          </p>
+        </li>
+        <li class="infos">
+          <p class="label">
+            Nous contacter
+          </p>
+          <p class="value boldy">
+            06 80 76 69 27
+          </p>
+        </li>
+        <li class="infos">
+          <p class="label">
+            Horaires d’ouverture
+          </p>
+          <p class="value boldy">
+            Du lundi au dimanche : 10h00 - 13h00, 14h00 - 19h00
+          </p>
+        </li>
+      </ul>
+      <h3>
+        Accès
+      </h3>
+      <ul class="infos-container list-in-rows-border">
+        <li class="infos">
+          <p class="label">
+            Voiture
+          </p>
+          <p class="value boldy">
+            Sortie 10 Rocade, remonter l’avenue Jean Perrin 
+            direction Chemin Long
+          </p>
+        </li>
+        <li class="infos">
+          <p class="label">
+            Bus
+          </p>
+          <p class="value boldy">
+            Bas ligne 1 lianes, direction Mérignac 
+            Aéroport, arrêt Kennedy
+          </p>
+        </li>
+        <li class="infos">
+          <p class="label">
+            Tramway
+          </p>
+          <p class="value boldy">
+            Prochainement
+          </p>
+        </li>
+      </ul>
   </div>
-  <div class="right-contact">
+  </div>
+  <div class="right-contact bkg-grey ">
   <?php echo do_shortcode('[gravityform id="1" title="false"]'); ?>
   </div>
 	
