2610515ede22c560eba7b54e1eb8834fb17bf3d6
mathias <mathias.duprat@live.com>
2023-06-16 16:15:47 +0200

    agence

---
+++ shortcodes/shortcode-equipe-card.php
@@ -0,0 +1,43 @@
+<?php
+function SHORTCODE_equipe_card($args)
+{
+    ob_start();
+    $atts = shortcode_atts( array(
+        'nom' => null,
+        'prenom' => null,
+        'photo' => null,
+        'description' => null,
+        'metier' => null,
+
+    ), $args );
+
+    $photo = $atts['photo'];
+    $nom = $atts['nom'];
+    $prenom = $atts['prenom'];
+    $description = $atts['description'];
+    $metier = $atts['metier'];
+
+    ?>
+
+    <div class="equipe-card">
+        <div class="equipe-card-content-container">
+            <img class="equipe-card-img" src="<?= $photo ?? " " ?>" alt="Image d'illustration marque"/>
+            <div class="equipe-card-container-paragraphe">
+                <p class="equipe-card-paragraphe"><?= $description ?? " " ?></p>
+            </div>
+        </div>
+        <div class="equipe-card-footer">
+            <h3 class="equipe-card-title"><?= $prenom ?? " " ?> <span><?= $nom ?? " " ?></span></h3>
+            <p class="equipe-card-metier"><?= $metier ?? " " ?></p>
+        </div>
+    </div>
+
+    <?php
+    return ob_get_clean();
+}
+
+add_shortcode('shortcode-equipe-card', 'SHORTCODE_equipe_card');
+
+
+
+?>
---
+++ shortcodes/shortcode-equipe-section.php
@@ -0,0 +1,37 @@
+<?php
+function SHORTCODE_Equipe_section()
+{
+    ob_start();
+    $args = array(
+        'post_type' => 'equipe',
+        'posts_per_page' => 1,
+        'orderby' => 'date',
+        'order' => 'DESC'
+    );
+    $query = new WP_Query($args);
+    if ($query->have_posts() ) :
+        $query->the_post();
+        $employes = get_field('employe');
+    ?>
+        <section class="cpt-section-container equipe-section-container">
+            <div class="cpt-section-header-title">
+                <p>PROFILS</p>
+                <div class="cpt-section-header-sub-content-container">
+                    <h2>Notre équipe</h2> 
+                    <a href="" class="cta-secondary">Rejoindre l'équipe</a>
+                </div>
+                
+            </div>
+            <div class="cpt-section-card-wrapper">
+                <?php 
+                foreach ($employes as $em) {
+                    echo do_shortcode('[shortcode-equipe-card nom="'.$em["nom"].'" prenom="'.$em["prenom"].'" photo="'.$em["photo"].'" description="'.$em["description"].'" metier="'.$em["metier"].'" ]');
+                } ?>
+            </div>
+        </section>
+
+    <?php endif; 
+    return ob_get_clean();
+}
+
+add_shortcode('shortcode-equipe-section', 'SHORTCODE_Equipe_section');
