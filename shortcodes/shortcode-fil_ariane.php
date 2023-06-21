96918a958d7d6cd2f16ce729da120a2c5981faac
Leon-maker <ldefonrocque@gmail.com>
2023-06-21 09:38:34 +0200

    modif

--- shortcodes/shortcode-fil_ariane.php
+++ shortcodes/shortcode-fil_ariane.php
@@ -1,3 +1,4 @@
+
 <?php
 function SHORTCODE_FilAriane($arguments)
 {
@@ -10,57 +11,68 @@ function SHORTCODE_FilAriane($arguments)
         "title2" => "",
         "link2" => "",
         "title3" => "",
-        "link3" => "",
-        "title4" => "",
-        "link4" => "",
+        "link3" => ""
     ), $arguments );
 
     $arrow_svg = wp_get_attachment_image_src(21228, 'full')[0];
-    $white_class="  ";
-    if($arguments_array['type'] === "white")$white_class="whited-filariane";
+    $white_class = "";
+    if ($arguments_array['type'] === "white") $white_class = "whited-filariane";
     ?>
- <div class="breadcrumb-container">
-    <nav class="fil-ariane-wrapper <?= $white_class ?>">
-        <ul class="fil-ariane-container list-in-row">
-            <?php
-            $nb_filled_arguments = 0;
+    <div class="breadcrumb-container">
+        <nav class="fil-ariane-wrapper <?= $white_class ?>">
+            <ul class="fil-ariane-container list-in-row">
+                <?php
+                $nb_filled_arguments = 0;
+                $lastLink = '';
 
-            // On compte le nombre d'arguments remplis
-            for ($i = 1; $i <= count($arguments_array) / 2; $i++) {
-                $title = $arguments_array['title' . $i];
-                $link = $arguments_array['link' . $i];
+                // On compte le nombre d'arguments remplis et on récupère le dernier lien
+                for ($i = 1; $i <= count($arguments_array) / 2; $i++) {
+                    $title = $arguments_array['title' . $i];
+                    $link = $arguments_array['link' . $i];
 
-                if ($title !== "") {
-                    $nb_filled_arguments++;
+                    if ($title !== "") {
+                        $nb_filled_arguments++;
+                        $lastLink = $link;
+                    }
                 }
-            }
 
-            for ($i = 1; $i <= count($arguments_array) / 2; $i++) {
-                $title = $arguments_array['title' . $i];
-                $link = $arguments_array['link' . $i];
+                for ($i = 1; $i <= count($arguments_array) / 2; $i++) {
+                    $title = $arguments_array['title' . $i];
+                    $link = $arguments_array['link' . $i];
 
-                if ($title !== "") {
+                    if ($title !== "") {
+                        ?>
+                        <li class="fil-ariane-item">
+                            <a href="<?php echo $link; ?>" class="fil-ariane-link">
+                                <?php echo $title; ?>
+                            </a>
+                            <?php
+                            // Si ce n'est pas le dernier élément du fil d'ariane, on affiche la flèche
+                            if ($i !== $nb_filled_arguments) {
+                                ?>
+                                <p class="dot-filariane"></p>
+                                <?php
+                            }
+                            ?>
+                        </li>
+                        <?php
+                    }
+                }
+
+                // Ajouter le dernier lien s'il existe
+                if (!empty($lastLink)) {
                     ?>
                     <li class="fil-ariane-item">
-                        <a href="<?php echo $link; ?>" class="fil-ariane-link">
-                            <?php echo $title; ?>
+                        <a href="<?php echo $lastLink; ?>" class="fil-ariane-link">
+                            <?php echo get_the_title(); ?>
                         </a>
-                        <?php
-                        // Si ce n'est pas le dernier élément du fil d'ariane, on affiche la flèche
-                        if ($i !== $nb_filled_arguments) {
-                            ?>
-                            <p class="dot-filariane"></p>
-                            <?php
-                        }
-                        ?>
                     </li>
                     <?php
                 }
-            }
-            ?>
-        </ul>
-    </nav>
-</div>
+                ?>
+            </ul>
+        </nav>
+    </div>
     <?php
     return ob_get_clean();
 }
