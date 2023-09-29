49525e94330ea0f37919d6dbcd0dcdab8f4a91e4
Matis <matis.bouquin@etu.u-bordeaux.fr>
2023-09-29 13:13:49 +0200

    seo

--- header.php
+++ header.php
@@ -1,7 +1,13 @@
 <!DOCTYPE HTML>
 <html <?php language_attributes(); ?>>
 <head>
-    
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MQRGVF6D');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -9,16 +15,22 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NC4PT7V7F4"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XMTGCZY3WK"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-NC4PT7V7F4');
+
+        gtag('config', 'G-XMTGCZY3WK');
     </script>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQRGVF6D"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 
 <?php wp_body_open(); 
 
